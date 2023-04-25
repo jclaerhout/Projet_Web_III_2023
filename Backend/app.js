0d37b0045da2baa2553963eaec128686fe303d36
Slywoud <he202160@students.ephec.be>
2023-04-26 00:55:45 +0200

    Update de la fonction getData + possiblité de faire plusieurs reqêtes vers la DB

--- Backend/app.js
+++ Backend/app.js
@@ -16,28 +16,43 @@ app.use('/accueil', (req, res, next) => {
 });
 
 // Route API qui correspond à la récupération des données users
-function getData(route, table) {
-  app.get(route, async (req, res) => {
-    let conn;
-    try {
-      conn = await pool.getConnection();
-      const rows = await conn.query(`SELECT * FROM ${table}`);
-      console.log(rows);
-      const jsonS = JSON.stringify(rows);
-      res.writeHead(200, {'Content-type': 'text/html'});
-      res.end(jsonS);
-    } catch (e) {
-      console.log(e);
-    } finally {
-      if (conn) conn.release();
-    }
-  });
+async function getData(query){
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    const rows = await conn.query(query);
+    console.log(rows);
+    const json = JSON.stringify(rows);
+    return json
+  } catch (e) {
+    console.log(e);
+  } finally {
+    if (conn) conn.release();
+  }
 }
 
-getData('', 'users')
+app.get('', async (req, res) => {
+  const query = 'SELECT * from comments';
+  const result = await getData(query);
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.end(result);
+});
+
+app.get('/profil', async (req, res) => {
+  const query = 'SELECT * from users';
+  const result = await getData(query);
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.end(result);
+});
 
 app.use('/login', (req, res, next) => {
   const message = 'Bienvenue la page de login du serveur Express !';
+  res.status(200).json(message);
+  next();
+});
+
+app.use('/profil', (req, res, next) => {
+  const message = 'Voici votre profil !';
   res.status(200).json(message);
   next();
 });
