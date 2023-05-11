140a0819b92cd20d63bf13738987697c83c4940a
Oscar <oscar.misson@gmail.com>
2023-05-11 09:44:27 +0200

    Deletion of the remaining search file

--- Backend/routes/search.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchCtrl = require('../controllers/search');
-
-router.get('/', searchCtrl.recherche);
-
-module.exports = router;
