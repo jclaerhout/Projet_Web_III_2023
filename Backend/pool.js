2f6295250d6d2e3d3d0b176259d159ae28471b56
“Your <vict918@gmail.com>
2023-05-28 14:05:04 +0200

    Test connection to web db

--- Backend/pool.js
+++ Backend/pool.js
@@ -1,9 +1,10 @@
 const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
+    host: 'mysql-6bb1d2c6-oc9d8d519.database.cloud.ovh.net',
+    port: 20184,
+    user: 'avnadmin',
+    password: 'heRtW32zoNa98kjq5XFL',
     database: 'dev3',
     maxConnections: 20,
   });
