0b08547277659c8b6acaa290f6a1eec1cfb21841
Julien Claerhout <j.claerhout@students.ephec.be>
2023-05-29 16:01:31 +0200

    Correction champ localisation

--- Frontend/src/views/Completion-profil.vue
+++ Frontend/src/views/Completion-profil.vue
@@ -69,7 +69,7 @@
                   label="Localisation"
                   types="(cities)"
                   placeholder="Saisissez votre localisation"
-                  @:placechanged="autocompleteLocation"
+                  @placechanged="autocompleteLocation"
                 ></vue-google-autocomplete>
               </v-col>
 
@@ -158,33 +158,33 @@ methods: {
   },
 
   updateUser() {
-      axios.put('http://localhost:3000/api/user/updateUser', {
-        userId: this.userId,
-        firstname: this.user.firstname,
-        lastname: this.user.lastname,
-        birthdate: this.user.birthdate,
-        sexe: this.user.sexe,
-        description: this.user.description,
-        location: this.user.location,
-        favoriteEquipment: this.user.favoriteEquipment,
-        xpPro: this.user.xpPro
-      })
-      .then(response => {
-        console.log(response.data);
-        this.$router.push('/profil');
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    axios.put('http://localhost:3000/api/user/updateUser', {
+      userId: this.userId,
+      firstname: this.user.firstname,
+      lastname: this.user.lastname,
+      birthdate: this.user.birthdate,
+      sexe: this.user.sexe,
+      description: this.user.description,
+      location: this.user.location,
+      favoriteEquipment: this.user.favoriteEquipment,
+      xpPro: this.user.xpPro
+    })
+    .then(response => {
+      console.log('Response received');
+      console.log(response.data);
+      this.$router.push('/profil');
+    })
+    .catch(error => {
+      console.log(error);
+    });
   },
 
-  /**
-      * When the location found
-      * @param {Object} addressData Data of the found location
-      */
   autocompleteLocation(addressData) {
-    this.user.location = addressData.locality;
+    const city = addressData.locality;
+    const country = addressData.country;
+    this.user.location = city + ', ' + country;
   }
+
 }
 };
 </script>
