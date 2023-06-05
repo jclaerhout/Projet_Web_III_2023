617c7612c7958db5a0144d059aafe5fe4d6b60d1
Julien Claerhout <j.claerhout@students.ephec.be>
2023-06-05 23:48:07 +0200

    Update CORS

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/user');
 const path = require('path');
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://photo-pro.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
