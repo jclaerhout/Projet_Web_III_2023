187957bd2880759aafebd2cbce084f0e3852c551
Julien Claerhout <j.claerhout@students.ephec.be>
2023-06-07 12:12:39 +0200

    CORS is fucking working

--- Backend/app.js
+++ Backend/app.js
@@ -7,25 +7,6 @@ const pageRoutes = require('./routes/page');
 const userRoutes = require('./routes/user');
 const path = require('path');
 
-/*app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    return res.status(200).json({});
-  }
-  next();
-});*/
-
 app.use(cors(
   {
     origin: '*',
