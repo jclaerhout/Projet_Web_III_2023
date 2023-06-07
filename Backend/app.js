31c1ff38f3bb82881f1e1432f2a39b8e599b4ef0
Julien Claerhout <j.claerhout@students.ephec.be>
2023-06-07 11:09:27 +0200

    Update CORS

--- Backend/app.js
+++ Backend/app.js
@@ -7,18 +7,38 @@ const pageRoutes = require('./routes/page');
 const userRoutes = require('./routes/user');
 const path = require('path');
 
-app.use((req, res, next) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+/*app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-});
+});*/
 
 app.use(express.json());
 app.use(cors({
   origin: 'https://photo-pro.vercel.app/'
 }));
-
+app.use(allowCors(handler));
 app.use('/api/page',pageRoutes);
 app.use('/api/user', userRoutes);
 
