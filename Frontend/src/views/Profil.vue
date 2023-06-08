9deeac3f81ec84bb3463ce883253c8bc63a9fa38
“Your <vict918@gmail.com>
2023-06-08 02:01:16 +0200

    Changement Script SQL DB + GetGalleries + CreateGallery

--- Frontend/src/views/Profil.vue
+++ Frontend/src/views/Profil.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="profil">
-    <v-sheet
+    <div class="profil-infos">
+      <v-sheet
       elevation="12"
       max-width="1000"
       rounded="lg"
       width="100%"
       class="pa-4 text-center mx-auto"
     >
-      <h1>Your Profile</h1>
+      <h1>Mon profil</h1>
       <div v-if="loading">Loading...</div>
       <div v-else>
         <v-row>
@@ -90,19 +91,53 @@
             </v-card>
           </v-col>
         </v-row>
+        <v-btn
+          block
+          to="/completion-profil"
+          color="hsla(160, 100%, 37%, 1)"
+          size="large"
+          type="submit"
+          variant="elevated"
+        >
+          Compléter mon profil
+        </v-btn>
       </div>
     </v-sheet>
-    <br /><br />
-    <v-btn
-      block
-      to="/completion-profil"
-      color="primary"
-      size="large"
-      type="submit"
-      variant="elevated"
-    >
-      Compléter mon profil
-    </v-btn>
+    </div>
+    <div class="profil-gallery">
+      <h1>Mes Galleries Photos</h1>
+      <div class="dialog">
+        <v-dialog v-model="dialog" max-width="500px">
+          <v-card>
+            <v-card-title>
+              <span class="headline">Créer une gallerie de photos</span>
+            </v-card-title>
+            <v-card-text>
+              <v-text-field v-model="name" label="Nom de la gallerie"></v-text-field>
+              <v-text-field v-model="description" label="Description de la gallerie"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="hsla(160, 100%, 37%, 1)" text @click="closeDialog">Annuler</v-btn>
+              <v-btn color="hsla(160, 100%, 37%, 1)" text @click="createGallery">Ok</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-btn id="button" color="hsla(160, 100%, 37%, 1)" @click="openDialog">+ Gallerie</v-btn>
+      </div>
+      <!-- Section pour afficher les galleries de photos -->
+      <div class="list-gallery">
+        <v-chip v-for="gallery in galleries" :key="gallery.id" @click="filterPhotosByGallery(gallery)">
+          {{ gallery.name }}
+        </v-chip>
+      </div>
+      <!-- Section pour afficher les photos filtrées -->
+      <div class="filtered-photos">
+        <v-card v-for="photo in filteredPhotos" :key="photo.id">
+          <!-- Affichage de la photo -->
+        </v-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -117,34 +152,44 @@ export default {
       loading: true,
       dialog: false,
       name: '',
-      description: ''
+      description: '',
+      galleries: [],
+      filteredPhotos: []
     }
   },
 
   created() {
-    const token = localStorage.getItem('token');
-
-    axios
-      .get('http://localhost:3000/api/page/profil', {
-        headers: { authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        this.user = response.data;
-        if (this.user[0].birthdate === null) {
-          this.user[0].birthdate = '';
-        } else {
-          const dateObj = new Date(this.user[0].birthdate);
-          const formatedDate = `${dateObj.getDate()}-${dateObj.getMonth()}-${dateObj.getFullYear()}`;
-          this.user[0].birthdate = formatedDate;
-        }
-        this.loading = false;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    this.fetchUser();
   },
 
   methods: {
+    // Méthode pour récupérer l'utilisateur connecté depuis le backend
+    fetchUser() {
+      const token = localStorage.getItem('token');
+
+      axios
+        .get('http://localhost:3000/api/page/profil', {
+          headers: { authorization: `Bearer ${token}` },
+        })
+        .then((response) => {
+          this.user = response.data;
+          if (this.user[0].birthdate === null) {
+            this.user[0].birthdate = '';
+          } else {
+            const dateObj = new Date(this.user[0].birthdate);
+            const formatedDate = `${dateObj.getDate()}-${dateObj.getMonth()}-${dateObj.getFullYear()}`;
+            this.user[0].birthdate = formatedDate;
+          }
+          this.loading = false;
+
+          // Récupération des galleries de photos de l'utilisateur
+          this.fetchGalleries(this.user[0].id);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+
     // Méthode pour ouvrir le dialog
     openDialog() {
       this.dialog = true
@@ -153,22 +198,40 @@ export default {
     // Méthode pour fermer le dialog
     closeDialog() {
       this.dialog = false
+      this.name = ''
+      this.description = ''
     },
 
-    // Méthode pour créer une galerie
+    // Méthode pour ajouter une gallerie a la db
     createGallery() {
-      const token = localStorage.getItem('token');
+      axios.post('http://localhost:3000/api/galleries', {
+        name: this.name,
+        description: this.description,
+        id_user: this.user[0].id
+      })
+      .then((response) => {
+        console.log(response)
+        const newGallery = response.data
+        this.galleries.push(newGallery)
+        this.closeDialog()
 
+        // Récupération des galeries mises à jour de l'utilisateur
+        this.fetchGalleries(this.user[0].id);
+      })
+      .catch (error => {
+        console.error(error)
+      });
+    },
+
+    // Méthode pour récupérer les galeries de l'utilisateur connecté depuis le backend
+    fetchGalleries(userId) {
+      const token = localStorage.getItem('token');
       axios
-        .post('http://localhost:3000/api/galleries', {
-          name: this.name,
-          description: this.description
-        }, {
-          headers: { Authorization: `Bearer ${token}` }
+        .get(`http://localhost:3000/api/galleries/${userId}`, {
+          headers: { authorization: `Bearer ${token}` },
         })
         .then((response) => {
-          console.log(response.data);
-          this.closeDialog();
+          this.galleries = response.data;
         })
         .catch((error) => {
           console.error(error);
@@ -181,45 +244,29 @@ export default {
 
 <style lang="scss" scoped>
 .profil {
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-
-  .infos {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 100%;
-    padding: 1rem 2rem;
-
-    .text {
-      text-align: left;
-    }
-
-    #button {
-      margin-top: 1rem;
-    }
-  }
-}
-
-.gallerie {
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: start;
-  margin: 6rem 0;
+  margin-top: 4rem;
 
-  h1 {
-    text-align: left;
+  &-infos {
+    padding: 1rem 2rem;
+    width: 600px;
   }
 
-  &-category {
+  &-gallery {
     display: flex;
-    margin-top: 1.5rem;
-    flex-direction: row;
-    justify-content: space-between;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 4rem;
 
-    .v-card-title {
-      padding: 1rem;
+    .dialog {
+      #button {
+        margin-top: 1.5rem;
+      }
+    }
+
+    .list-gallery {
+      margin-top: 2rem;
     }
   }
 }
